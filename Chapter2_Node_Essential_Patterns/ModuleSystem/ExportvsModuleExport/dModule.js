d139a452fa822688575c64c2f4b259db50e2eecf
Juan Luis Perez <tipiwiny@gmail.com>
2018-02-06 23:29:38 +0100

    difference between module.exports and exports

---
+++ Chapter2_Node_Essential_Patterns/ModuleSystem/ExportvsModuleExport/dModule.js
@@ -0,0 +1,11 @@
+
+
+exports = () => {
+  console.log('Hello');
+}
+module.exports = () => {
+  console.log('Hello module exports');
+}
+exports.hello = () => {
+  console.log('Hello');
+}
---
+++ Chapter2_Node_Essential_Patterns/ModuleSystem/ExportvsModuleExport/fModule.js
@@ -0,0 +1,6 @@
+const dModule = require('./dModule');
+
+
+
+dModule()
+dModule.hello() // this not work
